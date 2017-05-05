1b52f3e7643ade02af8e162c1af10eb0682c972f
camcamd <camille.devaux@hotmail.fr>
2017-05-05 15:36:17 +0200

    Items and xpath declared - not functional

--- lafourchette/lafourchette/items.py
+++ lafourchette/lafourchette/items.py
@@ -10,5 +10,21 @@ import scrapy
 
 class LafourchetteItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+	website = scrapy.Field()   #par défaut
+	type_activite= scrapy.Field()    #par défaut
+	sous_type_activite = scrapy.Field()   #par défaut
+	language = scrapy.Field()   #par défaut
+	region = scrapy.Field()    #à définir
+	activity_title = scrapy.Field()
+	zipcode = scrapy.Field()
+	city = scrapy.Field()
+	nb_comments = scrapy.Field()
+	content = scrapy.Field()
+	positive_content = scrapy.Field()    #null par défaut
+	negative_content = scrapy.Field()    #null par défaut
+	date = scrapy.Field()
+	note = scrapy.Field()
+	note_cuisine = scrapy.Field()
+	note_service = scrapy.Field()
+	note_cadre = scrapy.Field()
+
--- lafourchette/lafourchette/pipelines.py
+++ lafourchette/lafourchette/pipelines.py
@@ -7,5 +7,23 @@
 
 
 class LafourchettePipeline(object):
-    def process_item(self, item, spider):
-        return item
+	def process_item(self, item, spider):
+		item.setdefault('website','la fourchette')
+		item.setdefault('type_activite','offre de services')
+		item.setdefault('sous_type_activite','restauration')
+		item.setdefault('language','fr')
+		item.setdefault('positive_content','')
+		item.setdefault('negative_content','')
+
+		item['region'] = item['zipcode']
+    	#item['zipcode']
+    	#item['city']
+    	#item['nb_comments']
+    	#item['date']
+
+		item['note']= float(item['note']) / 2
+		item['note_cuisine'] = float(item['note_cuisine']) / 2
+		item['note_service'] = float(item['note_service']) / 2
+		item['note_cadre'] = float(item['note_cadre']) / 2
+
+		return item
